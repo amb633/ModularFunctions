ac97fb757fb40b15181fb31111859763132c0c71
hm535 <hm535@users.noreply.github.com>
2018-05-12 17:08:32 -0400

    merge conflict resolved

--- local_approximation.cpp
+++ local_approximation.cpp
@@ -11,7 +11,7 @@ void generic_polynomial_function ( double time , vector<double>* input , vector<
 
 namespace local_approximation {
 
-	void forwardEuler( void (*function)(double time , vector<double>* , vector<double>* ) ,
+	void forwardEuler( void (*function)(double , vector<double>* input, vector<double>* ) ,
 		double time , double march , vector<double>* input , vector<double>* gradient )
 	{
 		vector<double> result_1 , result_2;
@@ -24,7 +24,7 @@ namespace local_approximation {
 		(*gradient).push_back( feu );
 	}
 
-	void backwardEuler( void (*function)( double time , vector<double>* , vector<double>* ) ,
+	void backwardEuler( void (*function)( double , vector<double>* , vector<double>* ) ,
 		double time , double march , vector<double>* input , vector<double>* gradient )
 	{
 		vector<double> result_1 , result_2;
@@ -38,7 +38,7 @@ namespace local_approximation {
 
 	}
 
-	void centralEuler ( void (*function)( double time , vector<double>* , vector<double>* ) , 
+	void centralEuler ( void (*function)( double , vector<double>* , vector<double>* ) , 
 		double time , double march , vector<double>* input , vector<double>* gradient )
 	{
 		vector<double> result_1 , result_2;
@@ -49,7 +49,18 @@ namespace local_approximation {
 
 		(*gradient).push_back( ceu );
 	}
-}
 
+	void secondTaylor( void (*function)(double , vector<double>* , vector<double>* ) ,
+		double time , double march , vector<double>* input , vector<double>* gradient )
+	{
+		vector<double> result_1 , result_2 , result_3;
+		function( time , input , &result_1 );
+		function( time + march , input , &result_2 );
+		function( time + 2.0 * march , input , &result_3 );
+
+		double sta = -(result_3[0]/(2.0*march)) - 3.0*(result_1[0]/(2.0*march)) + 2.0*result_2[0]/march;
+		(*gradient).push_back(sta);	
+	}
+}
 
 
